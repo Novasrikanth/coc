d3c9fc26a540f3df9376cbe8f50c7ad90313d9e5
Novasrikanth <srikanth.kuchipudi4@gmail.com>
2023-03-06 14:24:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_m0hA6F8Oxz43Ibv4LAafyLh57JYsa-hgsg&usqp=CAU"  className="App-logo" alt="logo" />
+        <p style={{color:"red"}}>
+          welcome to church of christ Hyderabad
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.churchofchristhyd.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Church of christ official web page
         </a>
+        <a href="https://www.youtube.com/@churchofchristhyd/channels" >Youtube</a>
+        <a href="https://www.facebook.com/groups/churchofchristhyd" >Facebook</a>
+        <a href= "mailto : kurrushdrk12gmail.com" target="_blank">Mail to</a>
+        
       </header>
     </div>
   );
